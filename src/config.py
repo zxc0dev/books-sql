8e457144f4e16050cc082ed1c0cd6282f647ef71
git-zxrcodev <zxrcodev@icloud.com>
2026-04-11 18:49:00 +0200

    update gitignore; add download and validator in initial load notebook; add DOWNLOAD_PATH in config;

--- src/config.py
+++ src/config.py
@@ -24,3 +24,5 @@ LOG_FILE = ROOT_DIR / "pipeline.log"
 CURRENT_YEAR = pd.Timestamp.now().year
 ISBN_REGEX = r"[\dX\-]{8,13}"
 ASIN_REGEX = r"B[\dA-Z]{9}"
+
+DOWNLOAD_PATH = "arashnic/book-recommendation-dataset"
