5a79b08d9518dd86bcef81ad598ac0ffe8e7505d
git-zxrcodev <zxrcodev@icloud.com>
2026-04-11 21:01:26 +0200

    update config with new variables for new db_setup.py;

--- src/config.py
+++ src/config.py
@@ -36,12 +36,14 @@ PROCESSED_DIR = ROOT_DIR / "data" / "02_processed"
 QUARANTINE_DIR = ROOT_DIR / "data" / "02_quarantine"
 LOG_FILE = ROOT_DIR / "logs" / f"pipeline_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
 
-SQL_LOAD = "sql/sql_load"     
-SQL_CREATE = "sql/sql_create"
+SQL_LOAD     = ROOT_DIR / "sql" / "sql_load"
+SQL_CREATE   = ROOT_DIR / "sql" / "sql_create"
+SQL_INDEXES  = ROOT_DIR / "sql" / "sql_indexes"
+SQL_TRIGGERS = ROOT_DIR / "sql" / "sql_triggers"
+SQL_VIEWS    = ROOT_DIR / "sql" / "sql_views"
 
 CURRENT_YEAR = pd.Timestamp.now().year
 ISBN_REGEX = r"[\dX\-]{8,13}"
 ASIN_REGEX = r"B[\dA-Z]{9}"
 
 DOWNLOAD_PATH = "arashnic/book-recommendation-dataset"
-
